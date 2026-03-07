f6da58506260ce9b0749d24d584623e4febc2842
qiuqiumaoearth <2016566910@qq.com>
2026-03-08 00:19:13 +0800

    改基础接口

--- src/utils/request.js
+++ src/utils/request.js
@@ -3,7 +3,7 @@ import { Toast } from 'vant'
 import store from '@/store'
 
 const instance = axios.create({
-  baseURL: 'http://smart-shop.itheima.net/index.php?s=/api',
+  baseURL: 'https://smart-shop.itheima.net/index.php?s=/api',
   timeout: 5000
 })
 
